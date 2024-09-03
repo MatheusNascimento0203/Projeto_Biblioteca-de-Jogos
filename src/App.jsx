1936cc4e08113f9dd74e617d1e5fe1b949a49029
Matheus Nascimento <matheusfla.12333@gmail.com>
2024-09-03 12:20:11 -0300

    Implementando metodos para salvar no local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
 
 function App() {
-  const [games, setGames] = useState([]);
+  const [games, setGames] = useState(() => {
+    const storedGames = localStorage.getItem("obc-game-lib");
+    if (!storedGames) return [];
+    return JSON.parse(storedGames);
+  });
   const [title, setTitle] = useState("");
   const [cover, setCover] = useState("");
 
   const addGame = ({ title, cover }) => {
     const id = Math.floor(Math.random() * 1000000);
     const game = { id, title, cover };
-    setGames((state) => [...state, game]);
+    setGames((state) => {
+      const newStateAdd = [...state, game];
+      localStorage.setItem("obc-game-lib", JSON.stringify(newStateAdd));
+      return newStateAdd;
+    });
+  };
+
+  const removeGame = (id) => {
+    setGames((state) => {
+      const newStateRemove = state.filter((game) => game.id !== id);
+      localStorage.setItem("obc-game-lib", JSON.stringify(newStateRemove));
+      return newStateRemove;
+    });
   };
 
   const handleSubmit = (ev) => {
@@ -59,7 +75,7 @@ function App() {
           Adicionar à biblioteca
         </button>
       </form>
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap flex-col">
         {games.map((game) => (
           <div key={game.id} className="flex items-center gap-8 mt-8 w-96">
             <img className=" w-40 object-cover" src={game.cover} alt="" />
@@ -67,7 +83,10 @@ function App() {
               <h2 className="m-0 pb-4 font-bold text-3xl text-white">
                 {game.title}
               </h2>
-              <button className="bg-[#1A1A1A]  w-36 rounded-lg text-white font-bold  hover:bg-gray-400 h-9">
+              <button
+                onClick={() => removeGame(game.id)}
+                className="bg-[#1A1A1A]  w-36 rounded-lg text-white font-bold  hover:bg-gray-400 h-9"
+              >
                 Remover
               </button>
             </div>
